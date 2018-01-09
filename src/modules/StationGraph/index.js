339c16a06d1494173e3b60abcb54512321f94fc5
siqister <siqi.zhu@gmail.com>
2018-01-08 20:37:43 -0500

    Create force layout for stationGraph view

--- src/modules/StationGraph/index.js
+++ src/modules/StationGraph/index.js
@@ -1,4 +1,4 @@
-import {nest, select, min, max, geoMercator} from 'd3';
+import {nest, select, min, max, geoMercator, scaleSqrt, forceSimulation, forceCollide, forceRadial} from 'd3';
 
 function StationGraph(dom){
 
@@ -6,19 +6,66 @@ function StationGraph(dom){
 	let _onUnmount = () => {};
 	let tripsData;
 	let stationsData;
+	let departuresByStation;
+	let arrivalsByStation;
 
+	//Reference to internal nodes
+	let svg;
+	let canvas;
 	//Projection
 	const projection = geoMercator()
 		.scale(180000)
 		.center([-71.081543, 42.348560]);
+	//Scale
+	const scaleSize = scaleSqrt()
+		.domain([0,3000])
+		.range([2,20]);
+	//Force layout
+	const force = forceSimulation();
+	const collide = forceCollide().radius(d => d.r + 2);
+	const radial = forceRadial();
 
 	function exports(data, stations){
 
 		//Recompute internal variables
-		_w = dom.clientWidth;
+		_w = dom.clientWidth - 30; //TODO: account for inner padding
 		_h = dom.clientHeight;
 
 		projection.translate([_w/2, _h/2]);
+
+		tripsData = data;
+		departuresByStation = nest()
+			.key(d => d.station0)
+			.rollup(xs => xs.length)
+			.map(tripsData);
+		arrivalsByStation = nest()
+			.key(d => d.station1)
+			.rollup(xs => xs.length)
+			.map(tripsData);
+		stationsData = stations.map(s => {
+			const [x,y] = projection([s.lng,s.lat]);
+			const departureVolume = departuresByStation.get(s.id_short)?departuresByStation.get(s.id_short):0;
+			const arrivalVolume = arrivalsByStation.get(s.id_short)?arrivalsByStation.get(s.id_short):0;
+			const r = scaleSize(departureVolume);
+			return {
+				...s,
+				departureVolume,
+				arrivalVolume,
+				r,
+				x,
+				y
+			}
+		});
+
+		//Update forces
+		radial
+			.x(_w/2)
+			.y(_h/2)
+			.radius(Math.min(_w, _h)/2-100);
+		force
+			.force('collide',collide)
+			.force('radial',radial)
+			.alpha(1);
 
 		redraw();
 	}
@@ -38,7 +85,32 @@ function StationGraph(dom){
 			.attr('class','station-graph module');
 
 		//Build/update visualization layers
-		
+		svg = root
+			.selectAll('.viz-layer-svg')
+			.data([1]);
+		svg = svg.enter()
+			.append('svg')
+			.attr('class','viz-layer-svg')
+			.merge(svg)
+			.attr('width',_w)
+			.attr('height',_h)
+			.style('position','absolute')
+			.style('top',0)
+			.style('left',0)
+			.call(renderStations);
+
+		canvas = root
+			.selectAll('.viz-layer-canvas')
+			.data([1]);
+		canvas = canvas.enter()
+			.append('canvas')
+			.attr('class','viz-layer-canvas')
+			.merge(canvas)
+			.attr('width',_w)
+			.attr('height',_h)
+			.style('position','absolute')
+			.style('top',0)
+			.style('left',0);
 
 		//Unmount button
 		let unmountButton = root
@@ -52,6 +124,26 @@ function StationGraph(dom){
 			.html('List view')
 			.on('click', _onUnmount);
 
+	}
+
+	function renderStations(_){
+
+		let stationNodes = _.selectAll('.station')
+			.data(stationsData, d => d.id_short);
+		stationNodes = stationNodes.enter()
+			.append('circle')
+			.attr('class','station')
+			.attr('transform', d => `translate(${_w/2},${_h/2})`)
+			.merge(stationNodes)
+			.attr('r', d => d.r);
+
+		force
+			.on('tick', () => {
+				stationNodes
+					.attr('transform', d => `translate(${d.x},${d.y})`);
+			})
+			.nodes(stationsData)
+			.restart();
 	}
 
 	exports.onUnmount = function(fn){
