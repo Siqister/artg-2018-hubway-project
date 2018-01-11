ff4e7a4f34a19b6730186fafd055e40597831961
Siqi Zhu <szhu@sasaki.local>
2018-01-11 11:34:27 -0500

    Add station transition effect to stationGraph

--- src/modules/StationGraph/index.js
+++ src/modules/StationGraph/index.js
@@ -1,4 +1,4 @@
-import {nest, select, map, min, geoMercator, scaleSqrt, forceSimulation, forceCollide, forceRadial, path} from 'd3';
+import {nest, select, map, min, geoMercator, scaleSqrt, forceSimulation, forceCollide, forceRadial, path, dispatch} from 'd3';
 import {MainPath, ArcPath} from '../TripBalanceGraph';
 const crossfilter = require('crossfilter');
 const Matrix = require('transformation-matrix-js').Matrix; //https://www.npmjs.com/package/transformation-matrix-js
@@ -12,6 +12,7 @@ function StationGraph(dom){
 	let departuresByStation;
 	let arrivalsByStation;
 	let highlightStation;
+	let locationLookup = new Map();
 
 	//Reference to internal nodes
 	let svg;
@@ -21,7 +22,6 @@ function StationGraph(dom){
 	let ctxOffScreen;
 	let stationLinks;
 	let stationNodes;
-	let locationLookup = new Map();
 	//Projection
 	const projection = geoMercator()
 		.scale(180000)
@@ -38,10 +38,16 @@ function StationGraph(dom){
 	let arcPath, mainPath;
 
 	//Animation related
-	let cf;
-	let t0;
-	let t1;
-	let t;
+	let cf; //crossfilter
+	let t0; //crossfilter dimension
+	let t1; //crossfilter dimension
+	let t; //global state for current animation time
+	let tEndTrigger = new Date(4000,0,1);
+	let tStartTrigger = new Date(2000,0,1);
+	let tripToEnd; //reference to the first trip currently in progress to end
+
+	//Module internal event dispatch
+	const _dispatch = dispatch('trip:ended', 'trip:started');
 
 	function exports(data, stations){
 
@@ -178,6 +184,9 @@ function StationGraph(dom){
 
 	function renderStations(_){
 
+		//For each station, render a link and a node
+		//Links
+		//'d' attribute is updated on force.tick
 		stationLinks = _.selectAll('.station-link')
 			.data(stationsData, d => d.id_short);
 		stationLinks = stationLinks.enter()
@@ -187,6 +196,7 @@ function StationGraph(dom){
 			.merge(stationLinks)
 			.call(renderLinks, highlightStation);
 
+		//Nodes
 		stationNodes = _.selectAll('.station-node')
 			.data(stationsData, d => d.id_short);
 		const stationNodesEnter = stationNodes.enter()
@@ -213,6 +223,25 @@ function StationGraph(dom){
 			.text('')
 			.filter(d => d.r1 > 15)
 			.text(d => `${d.code3}`);
+
+		//Style nodes and links based on trip activity
+		_dispatch.on('trip:ended', trip => {
+			stationLinks.filter(d => d.id_short === trip.station1)
+				.style('fill','url(#grad1)');
+			stationNodes.filter(d => d.id_short === trip.station1)
+				.select('circle')
+				.attr('r', d => d.r1 + 2)
+				.transition()
+				.attr('r', d => d.r1 - 2)
+				.style('fill', 'white');
+		});
+		_dispatch.on('trip:started', trip => {
+			stationLinks.filter(d => d.id_short === trip.station1)
+				.style('fill','rgba(0,0,200,.5)');
+			stationNodes.filter(d => d.id_short === trip.station1)
+				.select('circle')
+				.style('fill', 'rgb(255,255,0)');
+		});
 
 		force
 			.on('tick', () => {
@@ -244,6 +273,7 @@ function StationGraph(dom){
 				t = min(tripsData, d => d.t0);
 
 				//For stations with arc path, compute offset from center
+				//Used to render trips
 				stationsData.forEach(s => {
 					if(!s.arc) return;
 					s.offsetFromCenter = Math.sqrt((s.x-_w/2)*(s.x-_w/2) + (s.y-_h/2)*(s.y-_h/2));
@@ -276,6 +306,12 @@ function StationGraph(dom){
 		t1.filter([t,Infinity]);
 		const tripsInProgress = t1.bottom(Infinity);
 
+		if(t > tEndTrigger){
+			_dispatch.call('trip:ended', null, tripToEnd);
+		}
+		tripToEnd = tripsInProgress[0];
+		tEndTrigger = tripToEnd?tripToEnd.t1:new Date(4000,0,1);
+
 		ctxOffScreen.clearRect(0,0,_w,_h);
 		ctxOffScreen.drawImage(canvas.node(),0,0);
 		ctx.clearRect(0,0,_w,_h);
@@ -295,21 +331,20 @@ function StationGraph(dom){
 
 		tripsInProgress.forEach(trip => {
 			//TODO: re-implement this based on a station at the center
+			//Current trip and stations
 			const {station0:s0, station1:s1, t0, t1} = trip;
-
 			if(!locationLookup.get(s0) || !locationLookup.get(s1)){
 				return;
 			}
-
 			const station0 = locationLookup.get(s0);
 			const station1 = locationLookup.get(s1);
+
 			const {x:x0,y:y0,arc:arc0} = station0;
 			const {x:x1,y:y1,arc:arc1} = station1;
-
 			const centerX = _w/2, centerY = _h/2;
 			const pct = (t.valueOf() - t0.valueOf())/(t1.valueOf() - t0.valueOf());
-			const path2d = trip.station1 === highlightStation? highlightPath2d : tripPath2d;
-			const r = trip.station1 === highlightStation? 3 : 3;
+			const path2d = s1 === highlightStation? highlightPath2d : tripPath2d;
+			const r = s1 === highlightStation? 3 : 3;
 			let _x, _y;
 
 			//Interpolate current trip location
@@ -352,31 +387,32 @@ function StationGraph(dom){
 				ctx.moveTo(offsetFromCenter, 0);
 				ctx.arc(offsetFromCenter/2,0,offsetFromCenter/2,0,Math.PI*(pct-1),true);
 				ctx.moveTo(tempX, tempY);
+				path2d.moveTo(_x+r, _y);
+				path2d.arc(_x, _y, r, 0, Math.PI*2);
 
 				//Reset
 				_m.reset();
 				_m.applyToContext(ctx);
-
-				path2d.moveTo(_x+r, _y);
-				path2d.arc(_x, _y, r, 0, Math.PI*2);
 			}	
 
-			//Outline target station
-			// const r1 = station1.r1;
-			// stationOutlinePath2d.moveTo(x1+r1+3,y1);
-			// stationOutlinePath2d.arc(x1,y1,r1+3,0,Math.PI*2);
+			//Trigger trip:started
+			if(trip.t0 > tStartTrigger){
+				tStartTrigger = trip.t0;
+				_dispatch.call('trip:started',null,trip);
+			}
+
 		});
 
+		//Stroking trip line
 		ctx.closePath();
 		ctx.strokeStyle = 'rgba(255,255,0,.2)';
 		ctx.stroke();
+		//Fill current trip location
 		ctx.fillStyle = 'rgb(255,255,0)';
 		ctx.fill(tripPath2d);
+		//Fill current trip location (highlighted)
 		ctx.fillStyle = 'rgba(255,255,255,1)';
 		ctx.fill(highlightPath2d);
-		// ctx.strokeStyle = 'rgb(255,255,0)';
-		// ctx.lineWidth = 2;
-		// ctx.stroke(stationOutlinePath2d);
 
 		//Update time and request next frame
 		t = new Date(t.valueOf() + 18000);
